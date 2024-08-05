7125b6bcc09cff1c3167ab9c6129847faaf37640
Seweryn <smicho01@student.bbk.ac.uk>
2024-08-05 23:48:12 +0100

    Add Blockchain Compose. Split env files to dev and prod

---
+++ README.md
@@ -0,0 +1,7 @@
+# AcademiChain Docker Compose
+
+This repository is used to deploy AcademiChain application to selected environment using Docker Compose
+
+## Commands
+
+`docker-compose --env-file .env.dev up -d`
--- sql/create-schema.sh
+++ sql/create-schema.sh
@@ -5,4 +5,5 @@ db_schema=$1
 
 export PGPASSWORD=$DATABASE_PASSWORD;
 
-psql postgresql://"$DATABASE_ENDPOINT":"$DATABASE_PORT"/"$DATABASE_NAME" -U "$DATABASE_USERNAME" -c "CREATE SCHEMA IF NOT EXISTS ${db_schema};"
+psql postgresql://"$DATABASE_ENDPOINT":"$DATABASE_PORT"/"$DATABASE_NAME" -U "$DATABASE_USERNAME" -c "CREATE SCHEMA IF NOT EXISTS ${db_schema}; SET timezone = 'Europe/London';
+ALTER DATABASE $DATABASE_NAME SET timezone = 'Europe/London';"
